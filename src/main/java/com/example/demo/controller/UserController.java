3ad69be0a030749f21d5aef587372b819d6c1442
bala-amypo <bala@amypo.in>
2025-12-19 08:20:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.UserEntity;
+import com.example.demo.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @PostMapping("/register")
+    public UserEntity register(@RequestBody UserEntity user) {
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+
+        return userRepository.save(user);
+    }
+}
