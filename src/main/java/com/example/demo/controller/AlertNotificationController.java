f481a820ee46e85643e82a348a7924fe0531165b
bala-amypo <bala@amypo.in>
2025-12-25 18:30:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/AlertNotificationController.java
+++ src/main/java/com/example/demo/controller/AlertNotificationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/alerts")
+@RequestMapping("/api")
 @Tag(name = "Alerts", description = "Alert notification APIs")
 public class AlertNotificationController {
 
