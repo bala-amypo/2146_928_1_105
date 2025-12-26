ef0bc68d135dbfea507129794a92feee36615f2f
bala-amypo <bala@amypo.in>
2025-12-26 00:24:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Visitor;
+import com.example.demo.entimodelty.Visitor;
 import com.example.demo.service.VisitorService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
